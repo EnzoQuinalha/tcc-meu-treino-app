883ba4880c79c89379e645aff9ed8a8a2136ffaf
Enzo Quinlha <enzoquinalha31@gmail.com>
2025-11-24 13:06:58 -0300

    criação do container HomePage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -10,5 +10,18 @@ import '../styles/base.css';
 function HomePage() {
   const [mensagem, setMensagem] = useState('Carregando dados...');
   const navigate = useNavigate();
+
+  return (
+    <>
+      <header className="page-header">
+        <h1>Bem vindo</h1>
+      </header>
+      <main className="page-content" style={{height: 'calc(100% - 23.7vh)', borderRadius: '0 0 0.75rem 0'}}>
+      </main>
+    </>
+  );
 }
+
+
+
 export default HomePage;
